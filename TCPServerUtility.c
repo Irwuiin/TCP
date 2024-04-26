3567b7abcf1d73f9f29829d0d0d53d02c30bafa4
Romero Irving (uidq7213) <Irving.Romero@continental-corporation.com>
2024-04-26 17:36:27 -0600

    Add messagecounter to TX/RX

--- TCPServerUtility.c
+++ TCPServerUtility.c
@@ -3,13 +3,15 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <string.h>
+#include <stdint.h>
 #include "Practical.h"
 
 extern FILE* fileStream;
 
 void HandleTCPClient(int clntSocket) {
     char buffer[BUFSIZE]; // Buffer for echo string
-
+    char ackMessage[] = " ACK";
     // Receive message from client
     ssize_t numBytesRcvd = recv(clntSocket, buffer, BUFSIZE, 0);
 #ifdef DEBUG
@@ -18,22 +20,23 @@ void HandleTCPClient(int clntSocket) {
     if (numBytesRcvd < 0) {
         DieWithSystemMessage("recv() failed");
     }
-
-    // Send received string and receive again until end of stream
+    uint8_t messageCounter = '0';
+    // Write data until end of stream
     while (numBytesRcvd > 0) { // 0 indicates end of stream
-        // Write data to file
+        messageCounter = (messageCounter % ':') ? messageCounter : '0';
         ssize_t numBytesWrt = fwrite(buffer, 1, numBytesRcvd, fileStream);
-        // Echo message back to client
-        //ssize_t numBytesSent = send(clntSocket, buffer, numBytesRcvd, 0);
+        // Send ACK message to client
+        ackMessage[0] = messageCounter;
+        ssize_t numBytesSent = send(clntSocket, ackMessage, strlen(ackMessage), 0);
 #ifdef DEBUG
-        //printf("Bytes sent: %d\n", (int)numBytesSent);
+        printf("Bytes sent: %d\n", (int)numBytesSent);
         printf("Bytes write: %d\n", (int)numBytesWrt);
 #endif
-        //if (numBytesSent < 0)
-            //DieWithSystemMessage("send() failed");
-        //else if (numBytesSent != numBytesRcvd)
+        if (numBytesSent < 0)
+            DieWithSystemMessage("send() failed");
+        else if (numBytesSent != numBytesRcvd)
         if (numBytesWrt != numBytesRcvd) {
-            //DieWithUserMessage("send()", "sent unexpected number of bytes");
+            DieWithUserMessage("send()", "sent unexpected number of bytes");
             DieWithUserMessage("fwrite()", "write unexpected number of bytes");
         }
 
@@ -45,6 +48,7 @@ void HandleTCPClient(int clntSocket) {
         if (numBytesRcvd < 0) {
             DieWithSystemMessage("recv() failed");
         }
+        messageCounter++;
     }
 
     if (fclose(fileStream) != 0) {
--- client.c
+++ client.c
@@ -6,17 +6,19 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <string.h>
 #include "Practical.h"
 
 #define BUFFER_LENGTH 100
 
-const char *echoString = "Hola mundo" ;
-
 int main(int argc, char *argv[]) {
 
-    if (argc < 3 || argc > 4) // Test for correct number of arguments
+    char ackMessage[] = " ACK";
+
+    if (argc < 3 || argc > 4) { // Test for correct number of arguments
         DieWithUserMessage("Parameter(s)",
         "<Server Address> <Echo Word> [<Server Port>]");
+    }
 
     char *servIP = argv[1]; // First arg: server IP address (dotted quad)
     char *filePath = argv[2]; // Second arg: file path
@@ -33,25 +35,28 @@ int main(int argc, char *argv[]) {
 
     // Create a reliable, stream socket using TCP
     int sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if (sock < 0)
+    if (sock < 0) {
         DieWithSystemMessage("socket() failed");
+    }
 
     // Construct the server address structure
     struct sockaddr_in servAddr; // Server address
     memset(&servAddr, 0, sizeof(servAddr)); // Zero out structure
     servAddr.sin_family = AF_INET; // IPv4 address family
     // Convert address
-
     int rtnVal = inet_pton(AF_INET, servIP, &servAddr.sin_addr.s_addr);
-    if (rtnVal == 0)
+    if (rtnVal == 0) {
         DieWithUserMessage("inet_pton() failed", "invalid address string");
-    else if (rtnVal < 0)
+    }
+    else if (rtnVal < 0) {
         DieWithSystemMessage("inet_pton() failed");
+    }
     servAddr.sin_port = htons(servPort); // Server port
 
     // Establish the connection to the echo server
-    if (connect(sock, (struct sockaddr *) &servAddr, sizeof(servAddr)) < 0)
+    if (connect(sock, (struct sockaddr *) &servAddr, sizeof(servAddr)) < 0) {
         DieWithSystemMessage("connect() failed");
+    }
 
     // Determine file size
     fseek(inputFile, 0, SEEK_END);
@@ -60,9 +65,7 @@ int main(int argc, char *argv[]) {
     printf("Input file size: %d\n", (int)remainingBytes);
 #endif
     rewind(inputFile);
-
-    //ssize_t echoStringLen = strlen(echoString); // Determine input length
-
+    uint8_t messageCounter = '0';
     // Start sending file to the server
     while(remainingBytes > 0) {
         char readBuffer[BUFFER_LENGTH];
@@ -81,7 +84,6 @@ int main(int argc, char *argv[]) {
                 DieWithSystemMessage("fread() error");
             }
         }
-        //ssize_t numBytes = send(sock, echoString, echoStringLen, 0);
         ssize_t numBytes = send(sock, readBuffer, bytesRead, 0);
         remainingBytes -= numBytes;
     #ifdef DEBUG
@@ -89,29 +91,36 @@ int main(int argc, char *argv[]) {
     #endif
         if (numBytes < 0)
             DieWithSystemMessage("send() failed");
-        //else if (numBytes != echoStringLen)
         else if (numBytes != bytesRead)
             DieWithUserMessage("send()", "sent unexpected number of bytes");
 
-        // Receive the same string back from the server
-        //unsigned int totalBytesRcvd = 0; // Count of total bytes received
-        //fputs("Received: \n", stdout); // Setup to print the echoed string
-        //while (totalBytesRcvd < echoStringLen) {
-        //while (totalBytesRcvd < bytesRead) {
-            //char buffer[BUFSIZE]; // I/O buffer
+        // Receive ACK from the server
+        unsigned int totalBytesRcvd = 0; // Count of total bytes received
+        fputs("Received:\n", stdout);
+        while (totalBytesRcvd < strlen(ackMessage)) {
+            char buffer[BUFSIZE]; // I/O buffer
             /* Receive up to the buffer size (minus 1 to leave space for
             a null terminator) bytes from the sender */
-            //numBytes = recv(sock, buffer, BUFSIZE - 1, 0);
-            //if (numBytes < 0)
-                //DieWithSystemMessage("recv() failed");
-            //else if (numBytes == 0)
-                //DieWithUserMessage("recv()", "connection closed prematurely");
-            //totalBytesRcvd += numBytes; // Keep tally of total bytes
-            //buffer[numBytes] = '\0'; // Terminate the string!
-            //fputs(buffer, stdout); // Print the echo buffer
-        //}
-
-        //fputc('\n', stdout); // Print a final linefeed
+            numBytes = recv(sock, buffer, BUFSIZE - 1, 0);
+            if (numBytes < 0) {
+                DieWithSystemMessage("recv() failed");
+            }
+            else if (numBytes == 0) {
+                DieWithUserMessage("recv()", "connection closed prematurely");
+            }
+            totalBytesRcvd += numBytes; // Keep tally of total bytes
+            // Check counter block
+            messageCounter = (messageCounter % ':') ? messageCounter : '0';
+            if (messageCounter == buffer[0]) {
+                buffer[numBytes] = '\0'; // Terminate the string!
+                fputs(buffer, stdout); // Print the ACK message
+                messageCounter++;
+            }
+            else {
+                //TODO
+            }
+        }
+        fputc('\n', stdout); // Print a final linefeed
     }
 
     if (fclose(inputFile) != 0)
